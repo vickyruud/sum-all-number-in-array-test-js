d0f43e178fde407f7f445f0dd785fa97af9d1207
Aprajit Kumar <aprajit.k@gmail.com>
2021-08-02 17:30:32 +0000

    all tests passed for sum

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  array.forEach((item) => {
+    if(Array.isArray(item)){
+      total += sumItems(item);
+    }else{
+    total += item;
+    }
+  });
+  return total;
+
 }
 
 module.exports = sumItems;
